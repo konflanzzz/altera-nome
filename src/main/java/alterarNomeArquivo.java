d60792df5a856770d5d89cf01a791afec15d32cc
Fernando Konflanz <konflanz.info@gmail.com>
2021-05-27 17:09:33 -0300

    Limpeza de codigo

--- src/main/java/alterarNomeArquivo.java
+++ src/main/java/alterarNomeArquivo.java
@@ -10,55 +10,71 @@ import java.nio.file.WatchService;
 
 public class alterarNomeArquivo {
     public static void main(String[] args) throws IOException {
+
         WatchService watchService = FileSystems.getDefault().newWatchService();
-        String caminho_temp = "C:/Temp/";
-        File diretorio_temp = new File(caminho_temp);
-        if (!diretorio_temp.exists()) diretorio_temp.mkdirs();
+        
+	String caminho_temp = "C:/Temp/";
+	File diretorio_temp = new File(caminho_temp);
+	if (!diretorio_temp.exists()) diretorio_temp.mkdirs();
         if (!caminho_temp.endsWith("\\")) caminho_temp += "\\";
-        // String caminho_remessas = "C:/NSClientSuite/Remessas/";
-        String caminho_remessas = "C:/Remessas/";
+	// String caminho_remessas = "C:/NSClientSuite/Remessas/";
+        
+	String caminho_remessas = "C:/Remessas/";
         File diretorio_remessas = new File(caminho_remessas);
         if (!diretorio_remessas.exists()) diretorio_remessas.mkdirs();
         if (!caminho_remessas.endsWith("\\")) caminho_remessas += "\\";
         Path directory = Paths.get("C:/Temp");
-        WatchKey watchKey = directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+        
+	WatchKey watchKey = directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
 
         while (true) {
             for (WatchEvent<?> event : watchKey.pollEvents()) {
-                Path nomeArquivo = directory.resolve((Path) event.context());
-                File antigo = nomeArquivo.toFile();
+                
+		Path nomeArquivo = directory.resolve((Path) event.context());
+                
+		File antigo = nomeArquivo.toFile();
                 String nomeAntigo = antigo.getName();
                 String nomeNovo = nomeAntigo.replaceFirst("[.][^.]+$", "");
-                if (nomeNovo.matches(".*\\d.*")) {
+                
+		if (nomeNovo.matches(".*\\d.*")) {
                     nomeNovo = ("NFEEMISSAO_" + nomeAntigo);
                 }
-                if (nomeNovo.matches("CANC")) {
+                
+		if (nomeNovo.matches("CANC")) {
                     nomeNovo = "NFECANC_.txt";
                 }
-                if (nomeNovo.matches("CCE")) {
+                
+		if (nomeNovo.matches("CCE")) {
                     nomeNovo = "NFECCE_.txt";
                 }
-                if (nomeNovo.matches("SITU")) {
+                
+		if (nomeNovo.matches("SITU")) {
                     nomeNovo = "NFESITUACAO_.txt";
                 }
-                if (nomeNovo.matches("MAIL")) {
+                
+		if (nomeNovo.matches("MAIL")) {
                     nomeNovo = "NFEREENVIOEMAIL_.txt";
                 }
-                if (nomeNovo.matches("RIMP")) {
+                
+		if (nomeNovo.matches("RIMP")) {
                     nomeNovo = "NFEREIMPRIME_.txt";
                 }
-                if (nomeNovo.matches("REVE")) {
+                
+		if (nomeNovo.matches("REVE")) {
                     nomeNovo = "NFEREIMPRIMEEVENTO_.txt";
                 }
-                if (nomeNovo.matches("PREV")) {
+                
+		if (nomeNovo.matches("PREV")) {
                     nomeNovo = "NFEPREVIA_.txt";
                 }
-                File novo = new File(caminho_remessas + nomeNovo);
+                
+		File novo = new File(caminho_remessas + nomeNovo);
 
                 if (antigo.renameTo(novo)) {
                     System.exit(0);
                     System.out.println("O Arquivo foi renomeado");
-                } else {
+                } 
+		else {
                     System.out.println("O Arquivo não foi renomeado");
                 }
             }
